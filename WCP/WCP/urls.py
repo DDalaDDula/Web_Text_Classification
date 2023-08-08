bb10167ef8048cde76fa60fc1bfc0d1bcb129ff3
DDalaDDula <wnsrb8560@hanmail.net>
2023-08-08 16:58:05 +0900

    django_study started from 0....

--- WCP/WCP/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from ..labeling import views
-
-urlpatterns = [
-    path('label_sentence/', views.label_sentence, name='label_sentence'),
-]
--- WCP/labeling/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class LabeledSentence(models.Model):
-    sentence = models.TextField()
-    emotion = models.CharField(max_length=20)  # 예: 'Happy', 'Sad', 'Neutral'
-
-    def __str__(self):
-        return self.sentence
